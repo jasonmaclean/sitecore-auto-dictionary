29eb901ec81737ad043e7bd89a98847800bc2236
Jason MacLean <Jason.MacLean@mnp.ca>
2022-10-07 12:05:55 -0400

    Initial Commit

---
+++ src/AutoDictionary/Models/AutoDictionary.cs
@@ -0,0 +1,11 @@
+﻿namespace SitecoreFundamentals.AutoDictionary.Models
+{
+    public class DictionaryReportItem
+    {
+        public string Key { get; set; }
+        public string DefaultValue { get; set; }
+        public string ContextUrl { get; set; }
+        public string LanguageName { get; set; }
+        public string Path { get; set; }
+    }
+}
